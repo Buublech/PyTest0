d67df41e9ee2d9cf1a7f366f710910d4750fab63
Buublech <ibullat@gmail.com>
2025-04-16 01:11:26 +0300

    Add files via upload

---
+++ PythonP/Main.py
@@ -0,0 +1,32 @@
+import requests
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = 'сюда Токен свой надо написать'
+HODOR = {'Content-Type' : 'application/json', 'trainer_token' : TOKEN}
+
+
+body_create = {
+    "name": "БоберЪ",
+    "photo_id": 33
+}
+
+
+resp_create = requests.post(url = f'{URL}/pokemons', headers = HODOR, json = body_create)
+print(resp_create.json())
+
+pokemoshka = resp_create.json()['id']
+
+body_catch = {
+    "pokemon_id": pokemoshka
+}
+
+resp_catch = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HODOR, json = body_catch)
+print(resp_catch.json())
+
+body_change = {
+    "pokemon_id": pokemoshka,
+    "name": "Ежык",
+    "photo_id": 28
+}
+resp_change = requests.put(url = f'{URL}/pokemons', headers = HODOR, json = body_change)
+print(resp_change.json())
